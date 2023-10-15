97fa6b8729bc24d49f63a94e33ead07ea64dc71d
fluffysnowman <thefluffysnowmanyt@gmail.com>
2023-10-15 15:00:50 +0530

    added table of contents[/general]; added description to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,12 +13,31 @@
 </script>
 
 <!-- <div id="home-page-content-wrapper" class="text-center"> -->
-<div id="home-page-content-wrapper" class="text-left pl-4 pt-4">
+<div id="home-page-content-wrapper" class="text-left pl-4 pt-4 pr-8">
 	<h1 class="text-2xl">Welcom to the SnowDocs</h1>
-	<br>
-
-	<p class="text-left">Here you can find documetation and notes for random things along with some tips and tricks that can make your life easier and better (on a computer. not in real life. we all know that real programmers and computer nerds never touch grass)</p>
+	<br />
+	<p class="text-left">
+		Here you can find documetation and notes for random things along with some tips and tricks that
+		can make your life easier and better (on a computer. not in real life. we all know that real
+		programmers and computer nerds never touch grass)
+	</p>
+	<br />
+	<p>
+		Currently I'm adding all the content from <a href="fluffysnowman.github.io"
+			>fluffysnowman.github.io</a
+		> to this site. The homepage (this page) will remain empty till that is done.
+	</p>
+	<br />
+	<p>
+		I will also be adding my <code class="text-purple-500"><b><i>personal notes</i></b></code> to
+		this site which include things that you can't find on the internet or are poorly explained in
+		documentation. This also includes things from system/server administration to programming to
+		<i>cyber security</i>
+	</p>
+	<br />
+	<p>
+		For updates on this project star and watch the <a
+			href="https://github.com/fluffysnowman/snowdocs" class="underline text-green-500">github repository</a
+		>. Feel free to contribute to the project (I'll be adding contribution instructions later)
+	</p>
 </div>
-
-
-
---
+++ src/routes/general/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { TableOfContents, tocCrawler } from '@skeletonlabs/skeleton';
+</script> 
+
+
+<div use:tocCrawler={{ mode: 'generate' }} class="hidden">
+	<h2>Networking Basics</h2>
+	<h3>Netmsks</h3>
+	<h3>DHCP</h3>
+	<h3>DNS</h3>
+	<h2>Heading 3</h2>
+	<p>...</p>
+</div>
+
+<TableOfContents class="sticky top-6 pl-4" />
+
