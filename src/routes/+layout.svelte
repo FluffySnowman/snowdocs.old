f67d038e9bada79501802c867ee26a11a3fb0a07
fluffysnowman <thefluffysnowmanyt@gmail.com>
2023-10-15 13:31:05 +0530

    first commit I guess

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<script>
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- The app shell thing -->
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="header">
+		<!-- top navbar shit -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-2xl font-thin">SnowDocs</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/fluffysnowman/snowdocs"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<svelte:fragment slot="sidebarLeft">
+		<!-- sidebar navbar list -->
+		<nav class="list-nav">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/general">General</a></li>
+				<li><a href="/linux">Linux</a></li>
+				<li><a href="/sec">Cyber Security</a></li>
+			</ul>
+		</nav>
+		<!-- --- -->
+	</svelte:fragment>
+
+	<!-- Page Route shit -->
+
+	<slot />
+
+	<!-- end of page router -->
+
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	// code block import shit
+
+	/* 
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+	import hljs from 'highlight.js/lib/core';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	hljs.registerLanguage('javascript', javascript);
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	storeHighlightJs.set(hljs); 
+	*/
+</script>
+
+<!-- <div id="home-page-content-wrapper" class="text-center"> -->
+<div id="home-page-content-wrapper" class="text-left pl-4 pt-4">
+	<h1 class="text-2xl">Welcom to the SnowDocs</h1>
+	<br>
+
+	<p class="text-left">Here you can find documetation and notes for random things along with some tips and tricks that can make your life easier and better (on a computer. not in real life. we all know that real programmers and computer nerds never touch grass)</p>
+</div>
+
+
+
